59ca8254b25ea38536827ca401ff468256474f5f
Allen Hu <allenhu1029@gmail.com>
2021-02-18 13:29:02 +0800

    Add profile pic and name, get posts

--- lib/body/info.dart
+++ lib/body/info.dart
@@ -5,18 +5,33 @@ import 'package:provider/provider.dart';
 class InfoBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final facebookModel = Provider.of<FacebookModel>(context, listen: false);
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text("已登入"),
-          RaisedButton(
-            child: Text("登出"),
-            onPressed: () => facebookModel.logout(),
-          )
-        ],
-      ),
+    return Consumer<FacebookModel>(
+      builder: (context, facebookModel, child) => FutureBuilder(
+          future: facebookModel.getUserProfile(),
+          builder: (context, AsyncSnapshot<Map<String, String>> snapshot) {
+            if (snapshot.hasData)
+              return Center(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Image.network(snapshot.data["pictureUrl"]),
+                  Text(snapshot.data["name"]),
+                  RaisedButton(
+                    child: Text("登出"),
+                    onPressed: () => facebookModel.logout(),
+                  ),
+                  RaisedButton(
+                    child: Text("GET"),
+                    onPressed: () {
+                      facebookModel.getUserFeed();
+                    },
+                  )
+                ],
+              ));
+            else
+              return Center(child: CircularProgressIndicator());
+          }),
     );
   }
 }
--- lib/providers/facebook.dart
+++ lib/providers/facebook.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:http/http.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 class FacebookModel extends ChangeNotifier {
   bool isLoggedIn = false;
@@ -10,6 +14,7 @@ class FacebookModel extends ChangeNotifier {
   Future<void> init() async {
     preferences = await SharedPreferences.getInstance();
     final token = preferences.getString("accessToken");
+    print(token);
     if (token != null) {
       isLoggedIn = true;
       accessToken = token;
@@ -18,7 +23,7 @@ class FacebookModel extends ChangeNotifier {
 
   Future<FacebookLoginStatus> login() async {
     final facebookLogin = FacebookLogin();
-    final result = await facebookLogin.logIn(['email']);
+    final result = await facebookLogin.logIn(['email', 'user_posts']);
     if (result.status == FacebookLoginStatus.loggedIn) {
       accessToken = result.accessToken.token;
       isLoggedIn = true;
@@ -38,5 +43,22 @@ class FacebookModel extends ChangeNotifier {
     preferences.remove("accessToken");
     isLoggedIn = false;
     notifyListeners();
+  }
+
+  Future<Map<String, String>> getUserProfile() async {
+    final response = await http.get(
+        'https://graph.facebook.com/v9.0/me?fields=name,picture&access_token=${accessToken}');
+    final decoded = jsonDecode(response.body);
+    return {
+      'name': decoded['name'],
+      'pictureUrl': decoded['picture']['data']['url']
+    };
+  }
+
+  Future<void> getUserFeed() async {
+    final response = await http.get(
+        'https://graph.facebook.com/v9.0/me/feed?fields=message&access_token=${accessToken}');
+    final decoded = jsonDecode(response.body);
+    print(decoded);
   }
 }
